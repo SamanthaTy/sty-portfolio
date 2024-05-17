35ea7a9b28cead50e547726bc425600a03d56c12
Samantha Ty <samantha@jedioftheco.de>
2024-05-17 20:30:51 +0200

    fix: created nvmrc file with v16.20.2

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: '#',
+  title: 'STY',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Samantha Ty',
+  role: 'Développeur Fullstack',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: '#',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/samantha-ty-johnson/',
+    github: 'https://github.com/SamanthaTy',
   },
 }
 
@@ -56,16 +56,17 @@ const skills = [
   'React',
   'Redux',
   'SASS',
-  'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Node.js',
+  'Express.js',
+  'Vite.js',
+  'PostgreSQL',
+  'Sequelize',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'ty.samantha@mail.com',
 }
 
 export { header, about, projects, skills, contact }
